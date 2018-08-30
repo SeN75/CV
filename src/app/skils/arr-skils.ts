cba8868df855cd376d48293a088b7ffe077db12c
SeN75 <saa111954@hotmail.com>
2018-08-30 15:07:24 +0300

    skils

--- src/app/skils/arr-skils.ts
+++ src/app/skils/arr-skils.ts
@@ -1,20 +1,24 @@
 
 export class SKILS {
     name: string;
-    width: number;
+    width: string ;
+    value: string;
 }
 export const SKIL: SKILS[] = [
-    { name: 'html', width: 100},
-    { name: 'css', width: 85},
-    { name: 'java script', width: 85},
-    { name: 'jquery', width: 60},
-    { name: 'c#', width: 70},
-    { name: 'node.js', width: 70},
-    { name: 'git', width: 80},
-    { name: 'xamarin', width: 70},
-    { name: 'react & react native', width: 60},
-    { name: 'angular', width: 70},
-    { name: 'bootstrap', width: 70},
-    { name: 'foundation', width: 90},
-    { name: 'cordova', width: 90},
-]
+    { name: 'html', width: 'width-100', value: '100%'},
+    { name: 'css', width: 'width-85', value: '85%'},
+    { name: 'java script', width: 'width-85', value: '85%'},
+    { name: 'jquery', width: 'width-60', value: '60%'},
+    { name: 'c#', width: 'width-70', value: '70%'},
+    { name: 'node.js', width: 'width-70', value: '70%'},
+    { name: 'git', width: 'width-85', value: '85%'},
+];
+
+export const SKIL2: SKILS[] = [
+    { name: 'xamarin', width: 'width-70', value: '70%'},
+    { name: 'react & react native', width: 'width-60', value: '60%'},
+    { name: 'angular', width: 'width-70', value: '70%'},
+    { name: 'bootstrap', width: 'width-70', value: '70%'},
+    { name: 'foundation', width: 'width-90', value: '90%'},
+    { name: 'cordova', width: 'width-85', value: '85%'}
+];
--- src/app/skils/skils.component.ts
+++ src/app/skils/skils.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {SKIL} from "./arr-skils";
+import { SKIL, SKIL2 } from './arr-skils';
+import { style } from '@angular/animations';
 @Component({
   selector: 'app-skils',
   templateUrl: './skils.component.html',
   styleUrls: ['./skils.component.css']
 })
 export class SkilsComponent implements OnInit {
- skils = SKIL;
+  skils = SKIL;
+  skils2 = SKIL2;
   constructor() { }
 
   ngOnInit() {
   }
-
+  setWidth(widht) {
+    const styles = {
+      'width': [widht]
+    };
+    console.log(widht);
+    return styles;
+  }
 }
