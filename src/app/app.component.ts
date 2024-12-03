766b574e41c55660982d4112adeb60a3ad6a8591
SeN75 <saa111954@hotmail.com>
2024-12-03 09:52:59 +0300

    update: update project angular cli to v18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,18 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HelperService } from './service/helper.service';
-import { ColorsSchemeService } from 'colors-scheme';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
   title = 'CV';
   container_width = 100;
   // @ViewChild('bg_container') bg_container!: ElementRef;
-  constructor(public helperSrv: HelperService, router: Router, private colorSrv:ColorsSchemeService) {
-    router.navigateByUrl('/')
-    console.log('colors:: ', this.colorSrv.createSwatches('#66CC99'))
+  constructor(public helperSrv: HelperService, router: Router) {
+    router.navigateByUrl('/');
   }
-  ngAfterViewInit(): void {
-
-  }
+  ngAfterViewInit(): void {}
 }
