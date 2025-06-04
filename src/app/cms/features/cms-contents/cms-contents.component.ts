3cb5d48b3c63a115f8e97d778e2002edbc135cd5
SeN75 <saa111954@hotmail.com>
2025-06-04 08:02:33 +0300

    feat: update project cli to v20

--- src/app/cms/features/cms-contents/cms-contents.component.ts
+++ src/app/cms/features/cms-contents/cms-contents.component.ts
@@ -9,14 +9,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { InputTextModule } from 'primeng/inputtext';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { SelectModule } from 'primeng/select';
-import { CommonModule } from '@angular/common';
+
 import { TableModule } from 'primeng/table';
 import { ProgressBar } from 'primeng/progressbar';
 import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-cms-contents',
-  imports: [TableModule, HttpClientModule, CommonModule, InputTextModule, TagModule,
-    SelectModule, MultiSelectModule, ProgressBar, ButtonModule, IconFieldModule, InputIconModule],  templateUrl: './cms-contents.component.html',
+  imports: [TableModule, HttpClientModule, InputTextModule, TagModule, SelectModule, MultiSelectModule, ProgressBar, ButtonModule, IconFieldModule, InputIconModule],  templateUrl: './cms-contents.component.html',
   styleUrl: './cms-contents.component.scss'
 })
 export class CmsContentsComponent {
